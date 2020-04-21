538d562fe56a2e00d18770b0decdb1470cd4dd3d
AdamMigliore <adam.dire@gmail.com>
2020-04-20 20:26:07 -0400

    added more tests

--- src/Algorithmic_test.java
+++ src/Algorithmic_test.java
@@ -228,7 +228,6 @@ public class Algorithmic_test {
 		// Test 1: add a null tweet
 		System.out.print(".");
 		twitter.addTweet(null);
-		System.out.println("Make sure that you cannot add a null Tweet in Twitter.addTweet()!\n");
 		// Test 2: add a normal tweet
 		System.out.print(".");
 		Tweet tweet = new Tweet("Adam", "2017-03-03 03:35:38", "I love 250!");
@@ -236,12 +235,13 @@ public class Algorithmic_test {
 		assertEquals(tweet, twitter.latestTweetByAuthor("Adam"));
 
 		System.out.println("Done!");
+		System.out.println("* Make sure that you cannot add a null Tweet in Twitter.addTweet()!");
 		System.out.println("No errors with Twitter.addTweet()\n");
 	}
 	
 	private void testLatestTweetByAuthor() throws AssertEqualsException {
 
-		System.out.print("-> Checking Twitter.addTweet()");
+		System.out.print("-> Checking Twitter.latestTweetByAuthor()");
 		//Test 1: no tweets
 		System.out.print(".");
 		twitter = new Twitter( new ArrayList<Tweet>(), new ArrayList<String>());
@@ -260,15 +260,67 @@ public class Algorithmic_test {
 		assertEquals(tweet, twitter.latestTweetByAuthor("Adam"));
 		
 		System.out.println("Done!");
-		System.out.println("No errors with Twitter.latestTweetByAuthor()()\n");
+		System.out.println("No errors with Twitter.latestTweetByAuthor()\n");
 	}
 	
-	private void testTweetsByDate() {
+	private void testTweetsByDate() throws AssertEqualsException {
+		System.out.print("-> Checking Twitter.tweetsByDate()");
 		
+		//Test 1: null date
+		System.out.print(".");
+		twitter = new Twitter( new ArrayList<Tweet>(), new ArrayList<String>());
+		assertNull(twitter.tweetsByDate(null));
+		//Test 2: date is non-existent
+		fillTwitter();
+		System.out.print(".");
+		assertNull(twitter.tweetsByDate("1990-03-03"));
+		//Test 3: normal query
+		System.out.print(".");
+		ArrayList<Tweet> myTweets = new ArrayList<>();
+		myTweets.add(new Tweet("Adam", "2017-03-03 03:35:38", "1"));
+		myTweets.add(new Tweet("Adam", "2017-03-03 03:36:38", "2"));
+		myTweets.add(new Tweet("Adam", "2017-03-03 03:37:38", "3"));
+		twitter = new Twitter( myTweets, new ArrayList<String>());
+		assertEquals(myTweets, twitter.tweetsByDate("2017-03-03"));
+		
+		System.out.println("Done!");
+		System.out.println("No errors with Twitter.tweetsByDate()\n");
 	}
 	
-	private void testTrendingTopics() {
+	private void testTrendingTopics() throws AssertEqualsException {
+		ArrayList<Tweet> myTweets = new ArrayList<>();
+		myTweets.add(new Tweet("Adam", "2017-03-03 03:35:38", "Hi, Adam Adam "));
+		myTweets.add(new Tweet("Adam", "2017-03-03 03:36:38", "Hi, Felix Adam"));
+		myTweets.add(new Tweet("Adam", "2017-03-03 03:37:38", "Hi, Felix Felix Adam"));
+		twitter = new Twitter( myTweets, new ArrayList<String>());
 		
+		ArrayList<String> answerWithoutStop = new ArrayList<>();
+		answerWithoutStop.add("hi"); //should be 3
+		answerWithoutStop.add("adam"); //should be 3
+		answerWithoutStop.add("felix");// should be 2
+		
+		ArrayList<String> stopWords = new ArrayList<>();
+		stopWords.add("adam");
+		
+		ArrayList<String> answerWithStop = new ArrayList<>();
+		answerWithStop.add("hi"); //should be 3
+		answerWithStop.add("felix");// should be 2
+		
+		System.out.print("-> Checking Twitter.trendingTopics()");
+		
+		//Test 1: without stopwords
+		System.out.print(".");
+		assertEquals(answerWithoutStop, twitter.trendingTopics());
+		
+		//Test 2: with stopwords
+		System.out.print(".");
+		twitter = new Twitter( myTweets, stopWords);
+		assertEquals(answerWithStop, twitter.trendingTopics());
+		
+		System.out.println("Done!");
+		System.out.println("* Make sure to ignore case for a word");
+		System.out.println("* Make sure that a word in a Tweet is only counted once");
+		System.out.println("No errors with Twitter.trendingTopics()\n");
 	}
 	
 	// Helpers
